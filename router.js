ca640974b6a72aceca5beebd825fb577190a7c5a
Yves Agostini <agostini@univ-metz.fr>
2015-09-15 09:20:59 +0200

    manage error, refactoring, doc

--- router.js
+++ router.js
@@ -3,6 +3,8 @@
 // Routing
 //
 
+// filter auth access an wait for subscribe
+
 var filters = {
 
   /**
@@ -41,6 +43,8 @@ var filters = {
  }
 };
 
+// filter to wait method result
+
 var method = { 
   waitID : function () {
     var content = Vlans.findOne({_id: this.params._id});
@@ -62,27 +66,29 @@ var method = {
   }
 };
 
-Router.configure({
-//  layout: 'start',
-  loadingTemplate: 'loading',
-  notFoundTemplate: 'not_found',
-});
+// REST access, checks and create or update
 
-
-Router.map(function () {
-  this.route('start', {
-    path: '/',
-    onBeforeAction: [filters.authenticate,filters.wait],
-//    data: function() {return Vlans.find()}
-  });
-// REST  
-  this.route('/api/vlans/createorupdate', function() {
+var rest = function () {
     var access = Meteor.settings.RESTaccess;
     var headers = this.request.headers;
     var token = headers['x-auth-token'];
     var ip = headers['x-forwarded-for'];
-    if (ip) check(ip, String);
-    if (token) check(token, String);
+    if (ip) check(ip, String,
+        function(err) {
+            if (err) {
+            console.log('ERROR 500: (IP) ' + err.message);
+            throw new Meteor.Error(err.message, 'ERROR 500: (IP) ');
+            }
+        }
+        );
+    if (token) check(token, String,
+        function(err) {
+            if (err) {
+            console.log('ERROR 500: (token) ' + err.message);
+            throw new Meteor.Error(err.message, 'ERROR 500: (token) ');
+            }
+        }
+        );
 
     if ( token != access.token || access.ips.indexOf(ip) === -1 ) {
       this.response.statusCode = 401;
@@ -98,27 +104,69 @@ Router.map(function () {
     var content = data.content;
     var owners = new Array;
     (typeof data.owners === 'string') ? owners.push(data.owners): owners = data.owners;
-    check(extId, String);
-    check(nom, String);
-    check(routeur, String);
-    check(content, String);
-    check(owners, Array);
+    check(extId, String,
+        function(err) {
+            if (err) {
+            console.log('ERROR 500: ' + err.message);
+            throw new Meteor.Error(err.message, 'ERROR 500: ');
+            }
+        }
+    );
+    check(nom, String,
+        function(err) {
+            if (err) {
+            console.log('ERROR 500: ' + err.message);
+            throw new Meteor.Error(err.message, 'ERROR 500: ');
+            }
+        }
+    );
+    check(routeur, String,
+        function(err) {
+            if (err) {
+            console.log('ERROR 500: ' + err.message);
+            throw new Meteor.Error(err.message, 'ERROR 500: ');
+            }
+        }
+    );
+    check(content, String,
+        function(err) {
+            if (err) {
+            console.log('ERROR 500: ' + err.message);
+            throw new Meteor.Error(err.message, 'ERROR 500: ');
+            }
+        }
+    );
+    check(owners, Array,
+        function(err) {
+            if (err) {
+            console.log('ERROR 500: ' + err.message);
+            throw new Meteor.Error(err.message, 'ERROR 500: ');
+            }
+        }
+    );
 
     var vlan = Vlans.findOne({'extId': extId});
 
     var response;
     if (vlan) {
       response = 'update';
-      Vlans.update({
-        "_id": vlan._id }, {
-        $set: {
+      Vlans.update({"_id": vlan._id },
+        {$set: {
            "nom": nom,
            "routeur": routeur,
            "content" : content,
            "owner" : owners,
            "modified": new Date,
-           "modifiedBy": 'REST@api'
-        }} ,{"getAutoValues": false} );
+           "modifiedBy": 'REST@api'}
+        },
+        {"getAutoValues": false},
+        function(err) {
+            if (err) {
+            console.log('ERROR 500: ' + err.message);
+            throw new Meteor.Error(err.message, 'ERROR 500: ');
+            }
+        }
+        );
     }
     else {
       response = 'create';
@@ -130,14 +178,42 @@ Router.map(function () {
            "owner" : owners,
            "created": new Date,
            "createdBy": 'REST@api'
-        },{"getAutoValues": false});
+        },
+        {"getAutoValues": false},
+        function(err) {
+            if (err) {
+            console.log('ERROR 500: ' + err.message);
+            throw new Meteor.Error(err.message, 'ERROR 500: ');
+            }
+        }
+        );
     };
     this.response.statusCode = 200;
     this.response.setHeader("Content-Type", "application/json");
     this.response.setHeader("Access-Control-Allow-Origin", "*");
     this.response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     this.response.end('OK ' + response );
-  }, {where: 'server'});
+};
+
+
+// Config
+
+Router.configure({
+//  layout: 'start',
+  loadingTemplate: 'loading',
+  notFoundTemplate: 'not_found',
+});
+
+
+// Map urls
+
+Router.map(function () {
+  this.route('start', {
+    path: '/',
+    onBeforeAction: [filters.authenticate,filters.wait],
+  });
+  // REST
+  this.route('/api/vlans/createorupdate', rest , {where: 'server'});
   // url to force a CAS auth
   this.route('cas', {
     path: '/cas',
